1897c8552408f26cf3bba73b60dbe3d32a81aca2
Danylo Zamkovyi <25032002ur@gmail.com>
2024-04-21 22:19:39 +0300

    add hw16

---
+++ HW16/script.js
@@ -0,0 +1,14 @@
+const clickButton = $(`.wrapper__btn`);
+const modalWindow = $(`.modal-window`);
+const closeButton = $(`.modal-window__btn`);
+clickButton.on(`click`, function () {
+  modalWindow.css(`display`, `flex`);
+  $(`body`).css(`overflow`, `hidden`);
+  $(`.wrapper`).css(`filter`, `blur(1px)`);
+  console.log($(document).scrollTop());
+});
+closeButton.on(`click`, function () {
+  modalWindow.css(`display`, `none`);
+  $(`body`).css(`overflow`, `auto`);
+  $(`.wrapper`).css(`filter`, `blur(0)`);
+});
