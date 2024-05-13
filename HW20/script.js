d3d23bc74437f54ea8bd2b715207ce73fb2b75e8
Danylo Zamkovyi <25032002ur@gmail.com>
2024-05-13 19:15:37 +0300

    add hw20

---
+++ HW20/script.js
@@ -0,0 +1,27 @@
+function boo() {
+  let i = 0;
+  return {
+    next: function () {
+      let value;
+      if (i < 3) {
+        i += 1;
+        value = i;
+        if (i === 1) {
+          console.log(`Start`);
+        }
+        return { value: value, done: false };
+      } else {
+        const result = { value: value, done: true };
+        i = 0;
+        console.log(`finish`);
+        return result;
+      }
+    },
+  };
+}
+
+const iterated = boo();
+console.log(iterated.next());
+console.log(iterated.next());
+console.log(iterated.next());
+console.log(iterated.next());
