310402ff44abf7ea88c827f0240aae7d1c5b6924
Danylo Zamkovyi <25032002ur@gmail.com>
2024-06-16 22:26:08 +0300

    changed place of request

--- admin-panel/src/index.js
+++ admin-panel/src/index.js
@@ -1,50 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./pages/Login/Login";
 import ProductsTable from "./pages/ProductsTable/ProductsTable";
 import ProductsPreview from "./pages/ProductsPreview/ProductsPreview";
 
-const App = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://666aa31b7013419182d0336a.mockapi.io/products"
-        );
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching the products:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-  return (
-    <div className="container">
-      {/* <Login /> */}
-      <ProductsTable products={products} />
-      {/* <ProductsPreview products={products} /> */}
-    </div>
-  );
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* <div className="container">
-      <Login />
-      <ProductsTable products={products} />
-      <ProductsPreview products={products} />
-    </div> */}
-    <App />
+    <div className="container">
+      {/* <Login /> */}
+      <ProductsTable />
+      {/* <ProductsPreview /> */}
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- admin-panel/src/pages/ProductsTable/ProductsTable.js
+++ admin-panel/src/pages/ProductsTable/ProductsTable.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ProductsTable.css";
 import Header from "./components/Header/Header";
 import Table from "../../components/Table/Table";
 
-function ProductTable({ products }) {
+function ProductTable() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://666aa31b7013419182d0336a.mockapi.io/products"
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching the products:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="container">
       <Header />
@@ -12,4 +29,5 @@ function ProductTable({ products }) {
     </div>
   );
 }
+
 export default ProductTable;
